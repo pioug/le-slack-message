90536d450a6da1b97a22a76880654a7bc22bf56b
Gilles Piou <gilles.piou@gmail.com>
2019-10-18 18:27:34 +0800

    Test Slack incoming webhook

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
 "use strict";
 
 const core = require("@actions/core");
+const { IncomingWebhook } = require("@slack/webhook");
 
 async function run() {
   try {
-    const github = core.getInput("github");
-    const job = core.getInput("job");
-    const steps = core.getInput("steps");
-    core.debug(github);
-    core.debug(job);
-    core.debug(steps);
-    core.setOutput("time", new Date().toTimeString());
+    const GITHUB = core.getInput("GITHUB", { required: true });
+    const JOB = core.getInput("JOB", { required: true });
+    const SLACK_WEBHOOK_URL = core.getInput("SLACK_WEBHOOK_URL", {
+      required: true
+    });
+
+    core.debug(GITHUB);
+    core.debug(JOB);
+
+    const message = new IncomingWebhook(SLACK_WEBHOOK_URL);
+    await message.send({
+      text: "Merci"
+    });
   } catch (error) {
     core.setFailed(error.message);
   }
